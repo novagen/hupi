56f5531e35c84159b1a46145503112592563578f
Christofer Engel <christofer.engel@pronon.se>
2020-10-08 10:09:47 +0200

    Array.equals

--- backend/audio/service.js
+++ backend/audio/service.js
@@ -5,6 +5,41 @@ import { Service, Constants } from 'wace-admin-service';
 import AsyncPolling from 'async-polling';
 import rpio from 'rpio';
 
+// Warn if overriding existing method
+if (Array.prototype.equals) {
+	console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
+}
+
+// attach the .equals method to Array's prototype to call it on any array
+Array.prototype.equals = (array) => {
+    // if the other array is a falsy value, return
+    if (!array)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (this.length != array.length){
+		return false;
+	}
+
+    for (var i = 0, l=this.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (this[i] instanceof Array && array[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!this[i].equals(array[i])) {
+				return false;
+			}
+        }           
+        else if (this[i] != array[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+};
+
+// Hide method from for-in loops
+Object.defineProperty(Array.prototype, "equals", { enumerable: false });
+
 const nats = new Service("Audio", Service.getNatsConfig(config)).connect();
 const portAudio = require('naudiodon');
 
@@ -114,12 +149,12 @@ const checkForRotation = (queue) => {
 		let clks = queue.map(i => i.clk);
 		let dts = queue.map(i => i.dt);
 
-		if (clks == clkUp && dts == dtUp) {
+		if (clks.equals(clkUp) && dts.equals(dtUp)) {
 			console.log('rotation up');
 			return;
 		}
 
-		if (clks == clkDown && dts == dtDown) {
+		if (clks.equals(clkDown) && dts.equals(dtDown)) {
 			console.log('rotation down');
 			return;
 		}
@@ -130,12 +165,12 @@ const checkForClick = (queue) => {
 	if (queue.length > 1) {
 		let pinData = queue.slice(queue.length - 3, queue.length - 1).map(i => i.sw);
 
-		if (pinData == [ 1, 0 ]) {
+		if (pinData,equals([ 1, 0 ])) {
 			console.log('click up');
 			return;
 		}
 
-		if (pinData == [ 0, 1 ]) {
+		if (pinData.equals([ 0, 1 ])) {
 			console.log('click down');
 			return;
 		}
