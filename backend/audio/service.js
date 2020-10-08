901a20631903b601cfd39679903fabbc981ec16a
Christofer Engel <christofer.engel@pronon.se>
2020-10-08 10:13:07 +0200

    More equals.

--- backend/audio/service.js
+++ backend/audio/service.js
@@ -5,40 +5,31 @@ import { Service, Constants } from 'wace-admin-service';
 import AsyncPolling from 'async-polling';
 import rpio from 'rpio';
 
-// Warn if overriding existing method
-if (Array.prototype.equals) {
-	console.warn("Overriding existing Array.prototype.equals. Possible causes: New API defines the method, there's a framework conflict or you've got double inclusions in your code.");
-}
-
-// attach the .equals method to Array's prototype to call it on any array
-Array.prototype.equals = (array) => {
+const equals = (current, array) => {
     // if the other array is a falsy value, return
     if (!array)
         return false;
 
     // compare lengths - can save a lot of time 
-    if (this.length != array.length){
+    if (current.length != array.length){
 		return false;
 	}
 
-    for (var i = 0, l=this.length; i < l; i++) {
+    for (var i = 0, l=current.length; i < l; i++) {
         // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
+        if (current[i] instanceof Array && array[i] instanceof Array) {
             // recurse into the nested arrays
-            if (!this[i].equals(array[i])) {
+            if (!current[i].equals(array[i])) {
 				return false;
 			}
         }           
-        else if (this[i] != array[i]) { 
+        else if (current[i] != array[i]) { 
             // Warning - two different object instances will never be equal: {x:20} != {x:20}
             return false;   
         }           
     }       
     return true;
 };
-
-// Hide method from for-in loops
-Object.defineProperty(Array.prototype, "equals", { enumerable: false });
 
 const nats = new Service("Audio", Service.getNatsConfig(config)).connect();
 const portAudio = require('naudiodon');
@@ -149,12 +140,12 @@ const checkForRotation = (queue) => {
 		let clks = queue.map(i => i.clk);
 		let dts = queue.map(i => i.dt);
 
-		if (clks.equals(clkUp) && dts.equals(dtUp)) {
+		if (equals(clks, clkUp) && equals(dts, dtUp)) {
 			console.log('rotation up');
 			return;
 		}
 
-		if (clks.equals(clkDown) && dts.equals(dtDown)) {
+		if (equals(clks, clkDown) && equals(dts, dtDown)) {
 			console.log('rotation down');
 			return;
 		}
@@ -165,12 +156,12 @@ const checkForClick = (queue) => {
 	if (queue.length > 1) {
 		let pinData = queue.slice(queue.length - 3, queue.length - 1).map(i => i.sw);
 
-		if (pinData,equals([ 1, 0 ])) {
+		if (equals(pinData, [ 1, 0 ])) {
 			console.log('click up');
 			return;
 		}
 
-		if (pinData.equals([ 0, 1 ])) {
+		if (equals(pinData, [ 0, 1 ])) {
 			console.log('click down');
 			return;
 		}
