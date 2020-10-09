35d0a33464dacbb7dacb50888fba0fe6eadad2da
Christofer Engel <christofer.engel@pronon.se>
2020-10-09 12:32:18 +0200

    Cleanup and breakup.

--- backend/audio/service.js
+++ backend/audio/service.js
@@ -2,11 +2,21 @@ import config from '../config';
 import diff from 'object-diff';
 import RotaryReader from './RotaryReader';
 import Service from '../service';
-import loudness from 'loudness';
+import VolumeControl from './VolumeControl';
 
 const nats = new Service("Audio", config.nats).connect();
 const portAudio = require('naudiodon');
 const stepSize = 1;
+
+const volume = new VolumeControl({
+	onSet: (changed) => {
+		Object.assign(volumeModel, changed);
+
+		nats.publish("event.audio.volume.change", JSON.stringify({
+			values: changed
+		}));
+	}
+});
 
 const volumeModel = {
 	volume: 50,
@@ -53,16 +63,11 @@ const toggleMute = () => {
 	const changed = diff(volumeModel, muted);
 
 	if (changed && Object.keys(changed).length) {
-		Object.assign(volumeModel, changed);
-
-		nats.publish("event.audio.volume.change", JSON.stringify({
-			values: changed
-		}));
+		volume.set(changed);
 	}
 };
 
 const setAudioValues = (val) => {
-	console.log('setAudioValues', val);
 	const changed = diff(volumeModel, val);
 
 	if (changed && Object.keys(changed).length) {
@@ -72,50 +77,6 @@ const setAudioValues = (val) => {
 			values: changed
 		}));
 	}
-};
-
-const getVolume = () => {
-	return new Promise((resolve, _) => {
-		loudness.getVolume().then(r => {
-			resolve(r);
-		}).catch(e => {
-			console.log(e);
-			resolve(0);
-		});
-	});
-};
-
-const getMuted = () => {
-	return new Promise((resolve, _) => {
-		loudness.getMuted().then(r => {
-			resolve(r);
-		}).catch(e => {
-			console.log(e);
-			resolve(false);
-		});
-	});
-};
-
-const setVolume = (val) => {
-	return new Promise((resolve, _) => {
-		loudness.setVolume(val).then(_ => {
-			resolve();
-		}).catch(e => {
-			console.log(e);
-			resolve();
-		});
-	});
-};
-
-const setMuted = (val) => {
-	return new Promise((resolve, _) => {
-		loudness.setMuted(val).then(_ => {
-			resolve();
-		}).catch(e => {
-			console.log(e);
-			resolve();
-		});
-	});
 };
 
 const changeVolume = (dir) => {
@@ -148,37 +109,15 @@ const changeVolume = (dir) => {
 	const changed = diff(volumeModel, volume);
 
 	if (changed && Object.keys(changed).length) {
-		updateVolumeModel(changed);
+		volume.update(changed);
 	}
-};
-
-const updateVolumeModel = (changed) => {
-	let promises = [];
-
-	if (changed.volume) {
-		promises.push(setVolume(changed.volume));
-	}
-
-	if (changed.mute) {
-		promises.push(setMuted(changed.mute));
-	}
-
-	Promise.all(promises).then(_ => {
-		Object.assign(volumeModel, changed);
-
-		nats.publish("event.audio.volume.change", JSON.stringify({
-			values: changed
-		}));
-	}).catch(e => {
-		console.error(e);
-	});
 };
 
 const init = () => {
 	let promises = [];
 
-	promises.push(getVolume());
-	promises.push(getMuted());
+	promises.push(volume.volume());
+	promises.push(volume.muted());
 
 	Promise.all(promises).then(r => {
 		let val = {
@@ -194,9 +133,15 @@ const init = () => {
 
 const read = () => {
 	new RotaryReader({
-		onRotation: (dir) => { changeVolume(dir); },
-		onClick: () => { toggleMute(); },
-		onError: (err) => { console.error(err); }
+		onRotation: (dir) => {
+			changeVolume(dir);
+		},
+		onClick: () => {
+			toggleMute();
+		},
+		onError: (err) => {
+			console.error(err);
+		}
 	}).start();
 };
 
@@ -219,13 +164,9 @@ nats.subscribe('get.audio.device.*', function (_, reply, subj) {
 });
 
 nats.subscribe('call.audio.volume.set', (req, reply) => {
-	const params = JSON.parse(req);
-	const changed = diff(volumeModel, params.params);
+	const params = JSON.parse(req).params;
 
-	if (changed && Object.keys(changed).length) {
-		updateVolumeModel(changed);
-	}
-
+	volume.set(params);
 	nats.publish(reply, Service.success());
 });
 
