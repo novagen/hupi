5197abee173e36db43f44ae5f262c24e81cfa894
Christofer Engel <christofer.engel@pronon.se>
2020-11-10 23:54:22 +0100

    Push location changes

--- backend/location/service.js
+++ backend/location/service.js
@@ -1,6 +1,7 @@
 import config from '../config';
 import Service from '../service';
 import Gpsd from 'node-gpsd-client';
+import diff from 'object-diff';
 
 const service = new Service("Audio", config.nats);
 
@@ -10,6 +11,20 @@ const client = new Gpsd({
 	autoReconnect: 5,
 	parse: true
 });
+
+const locationModel = {
+	time: '',
+	ept: 0,
+	lat: 0,
+	lon: 0,
+	alt: 0,
+	epx: 0,
+	epy: 0,
+	epv: 0,
+	track: 0,
+	speed: 0,
+	climb: 0
+};
 
 const init = () => {
     client.on('connected', () => {
@@ -31,13 +46,30 @@ const init = () => {
 
 const listen = () => {
     client.on('TPV', data => {
-        console.log(data);
-    });
-};
+        const val = {
+            time: data.time,
+            ept: data.ept,
+            lat: data.lat,
+            lon: data.lon,
+            alt: data.alt,
+            epx: data.epx,
+            epy: data.epy,
+            epv: data.epv,
+            track: data.track,
+            speed: data.speed,
+            climb: data.climb
+        };
 
-const locationModel = {
-	lat: 0,
-	lng: 0
+        const changed = diff(locationModel, val);
+
+        if (changed && Object.keys(changed).length) {
+            Object.assign(locationModel, changed);
+
+            service.publish("event.location.spot.change", {
+                values: changed
+            });
+        }
+    });
 };
 
 service.subscribe('get.location.spot', function (_, reply) {
